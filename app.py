99d03eb64874a852ce9db03dfe29249e1e76b2ff
Lyra Hall <lyrahall@gmail.com>
2018-03-06 14:10:36 -0800

    implemented flask-httpauth for protecting against unauthorized requests

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 #!/Users/lyrahall/src/flask_api/env/bin/python3.6
 # tutorial code from https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
@@ -28,7 +32,7 @@ def index():
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
@@ -37,7 +41,7 @@ def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 
 @app.errorhandler(404)
@@ -46,6 +50,7 @@ def not_found(error):
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
 def create_task():
     # to test: curl -i -H "Content-Type: application/json" -X POST -d '{"title":"Read a book"}' http://localhost:5000/todo/api/v1.0/tasks
     if not request.json or not 'title' in request.json:
@@ -61,6 +66,7 @@ def create_task():
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
     # to test: curl -i -H "Content-Type: application/json" -X PUT -d '{"done":true}' http://localhost:5000/todo/api/v1.0/tasks/2
     # this will update task 2's done value to true
@@ -78,16 +84,47 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
+    # to delete the second task: curl -i -H "Content-Type: application/json" -X DELETE http://localhost:5000/todo/api/v1.0/tasks/2
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
+
+
+@auth.get_password
+def get_password(username):
+    # to send credentials, add -u miguel:python to curl command
+    # only 1 user in this system
+    if username == 'miguel':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+#################################################################################
+# Helper functions
+
+
+def make_public_task(task):
+    # replaces the id field in each task with a uri for the user's convenience
+    # this only happens when the user makes a get request for the full list of tasks
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 
 if __name__ == '__main__':
